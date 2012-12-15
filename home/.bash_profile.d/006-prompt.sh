4ea8b32ef40813b9716182564f127997326299db
Kevin Burke <kembuco@gmail.com>
2012-12-14 18:09:38 -0700

    updated the prompt

--- home/.bash_profile.d/006-prompt.sh
+++ home/.bash_profile.d/006-prompt.sh
@@ -1,13 +1,2 @@
 # needs 001-colors.sh
-
-PS1_LINE1="${BRIGHT_GREEN}———————————————— macbook-pro \t ————————————————"
-
-if [[ -x ~/.rvm/bin/rvm-prompt && -x __git_ps1 ]]; then
-  PS1_LINE2="${BRIGHT_RED}\$(~/.rvm/bin/rvm-prompt)${RESET} in ${BRIGHT_YELLOW}\w${BRIGHT_CYAN} \$(__git_ps1 '(%s) ')"
-else
-  PS1_LINE2="${BRIGHT_YELLOW}\w"
-fi
-
-PS1_LINE3="${BRIGHT_BLUE}\$${RESET} "
-
-export  PS1="\n${PS1_LINE1}\n${PS1_LINE2}\n${PS1_LINE3}"
+export  PS1="${BRIGHT_YELLOW}\w ${BRIGHT_BLUE}\$${RESET}"
